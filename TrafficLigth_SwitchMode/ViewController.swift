9bd8f510ff53663e55d8773660ed51973ea890f1
Sergei Veresaev <138131897+veresaev@users.noreply.github.com>
2023-08-18 00:41:09 +0300

    Initial Commit

---
+++ TrafficLigth_SwitchMode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLigth_SwitchMode
+//
+//  Created by Sergey Veresaev on 18.8.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
